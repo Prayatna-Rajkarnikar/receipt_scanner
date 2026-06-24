d30deafd1c59140964d7615799cc706599fca6a1
Prayatna-Rajkarnikar <prayatna.rajkarnikar@gmail.com>
2026-06-24 16:44:13 +0545

    feat: add draft receipt to store

--- store/receipt.ts
+++ store/receipt.ts
@@ -4,12 +4,16 @@ import { create } from "zustand";
 type ReceiptStore = {
   receipts: Receipt[];
   addReceipt: (receipt: Receipt) => void;
+  draftReceipt: Receipt | null;
+  setDraft: (receipt: Receipt) => void;
 };
 
 const useReceipt = create<ReceiptStore>((set) => ({
   receipts: [],
   addReceipt: (receipt) =>
     set((state) => ({ receipts: [...state.receipts, receipt] })),
+  draftReceipt: null,
+  setDraft: (receipt) => set({ draftReceipt: receipt }),
 }));
 
 export default useReceipt;
